01fa3548dbccf2b402a170ea1bf795bba214fb44
Nijarudeen <nijarr2020@gmail.com>
2020-08-30 13:13:05 +0530

    New commit message.

---
+++ client/src/Chat.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache()
+});
