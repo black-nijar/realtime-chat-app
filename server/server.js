f00a653f1f534005e1c15ea4a9d6a77445ac5bc9
Nijarudeen <nijarr2020@gmail.com>
2020-09-06 18:34:27 +0530

    messages updated

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const { GraphQLServer } = require('graphql-yoga');
+const { GraphQLServer, PubSub } = require('graphql-yoga');
 
 const messages = [];
 
@@ -16,7 +16,14 @@ const typeDefs = `
  type Mutation {
    postMessage(user: String!, content: String!): ID!
  }
+
+ type Subscription {
+   messages: [Message!]
+ }
 `;
+
+const subscribers = [];
+const onMessageSend = (fn) => subscribers.push(fn);
 
 const resolvers = {
   Query: {
@@ -30,12 +37,23 @@ const resolvers = {
         user,
         content,
       });
+      subscribers.forEach(fn => fn())
       return id;
     },
   },
+  Subscription: {
+    messages: {
+      subscribe: (parent, args, { pubsub }) => {
+        const channel = Math.random().toString(36).slice(2, 15);
+        onMessageSend(() => pubsub.publish(channel, { messages }), 0)
+        return pubsub.asyncIterator(channel)
+      }
+    }
+  }
 };
+const pubsub = new PubSub();
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } });
 
 server.start(({ port }) => {
   console.log(`Server on http://localhost:${port}/`);
